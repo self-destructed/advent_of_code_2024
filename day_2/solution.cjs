90e1ac64373c77c97c89930c625deb3357eaea1f
self-destructed <self.destructed.dan@gmail.com>
2024-12-02 18:07:04 +0300

    refactor: optimize day2 solution

--- day_2/solution.cjs
+++ day_2/solution.cjs
@@ -1,54 +1,49 @@
 const _ = require('lodash/fp');
 const data = require('./data.json');
 
-// const data = [
-//   [7, 6, 4, 2, 1],
-//   [1, 2, 7, 8, 9],
-//   [9, 7, 6, 2, 1],
-//   [1, 3, 2, 4, 5],
-//   [8, 6, 4, 4, 1],
-//   [1, 3, 6, 7, 9],
-// ];
+const isOrderAscending = (array) => {
+  return array[0] < array[array.length - 1];
+};
 
-const isReportOrderAscending = (report) => {
-  return report[0] < report[report.length - 1];
+const isPairSafe = (prev, next, isAscending, min = 1, max = 3) => {
+  const diff = prev - next;
+  const absDiff = Math.abs(diff);
+
+  return (
+    (isAscending ? diff < 0 : diff > 0) && absDiff >= min && absDiff <= max
+  );
 };
 
 const isReportSafe = (report) => {
-  const isAscending = isReportOrderAscending(report);
+  const isAscending = isOrderAscending(report);
   for (let i = 0; i < report.length - 1; i += 1) {
-    const diff = report[i] - report[i + 1];
-    if ((isAscending && diff > 0) || (!isAscending && diff < 0)) {
+    if (!isPairSafe(report[i], report[i + 1], isAscending)) {
       return false;
     }
-
-    const absDiff = Math.abs(diff);
-    if (absDiff < 1 || absDiff > 3) return false;
   }
 
   return true;
 };
 
-const getSafeReports = (reports) => {
-  return reports.filter(isReportSafe);
-};
-
 const first = (reports) => {
-  return getSafeReports(reports).length;
+  return reports.filter(isReportSafe).length;
 };
 
 console.log('Answer1: ', first(data));
 
 const generateSubsets = (array) => {
-  return array.map((_item, index) =>
-    array.filter((_sameItem, i) => i !== index),
-  );
+  return array.map((_item, index) => [
+    ...array.slice(0, index),
+    ...array.slice(index + 1),
+  ]);
+};
+
+const isReportSafeWithRemoval = (report) => {
+  return isReportSafe(report) || generateSubsets(report).some(isReportSafe);
 };
 
 const second = (reports) => {
-  return reports.filter((report) => {
-    return isReportSafe(report) || generateSubsets(report).some(isReportSafe);
-  }).length;
+  return reports.filter(isReportSafeWithRemoval).length;
 };
 
 console.log('Answer2: ', second(data));
